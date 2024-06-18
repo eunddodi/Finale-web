1ed4e0f906f35c6a4602c733307344c9ccc315bc
Claire Lee <claire.lee@crashco.io>
2024-06-18 19:18:41 +0900

    :sparkles: `apiRequest` 함수가 토큰을 인자로 받도록 수정, 요일 포맷팅 함수 추가

--- src/util.tsx
+++ src/util.tsx
@@ -1,10 +1,13 @@
 import { API_ENDPOINT } from './constants'
 
-export async function apiRequest(url: string, options?: any) {
+export async function apiRequest(url: string, token?: string, options?: any) {
   try {
     const response = await fetch(`${API_ENDPOINT}/${url}`, {
       credentials: 'include',
       secure: false,
+      headers: {
+        'Authorization': 'Bearer ' + token,
+      },
       ...options
     })
 
@@ -15,5 +18,17 @@ export async function apiRequest(url: string, options?: any) {
   }
   catch (e) {
     console.error(e)
+    // TODO: 401, 403의 경우 rethrow하지 않고 redirect 처리
+    throw new Error(e)
   }
+}
+
+export function formatDayOfWeek(dayOfWeek: number): string {
+  const daysOfWeek: string[] = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+
+  if (dayOfWeek < 0 || dayOfWeek > 6) {
+    throw new Error('올바른 요일 값이 아닙니다. 0부터 6 사이의 값을 입력해주세요.');
+  }
+
+  return daysOfWeek[dayOfWeek];
 }
