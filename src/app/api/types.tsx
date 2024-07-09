23c705845695fbf18adafd397d251ae545859092
Claire Lee <claire.lee@crashco.io>
2024-07-09 21:36:18 +0900

    :alien: 레슨 DTO 업데이트 반영

--- src/app/api/types.tsx
+++ src/app/api/types.tsx
@@ -1,16 +1,17 @@
 export interface ILesson {
-  id: number
-  location: string
-  cost: string
-  day: number // 0:Sun-6:Sat
-  maxStudents: number
-  currentEnrollment: number
-  studentsPerCoach: number
-  lessonDates: {
-    date: string
-    startTime: string
-    endTime: string
-  }[]
+  id: number;
+  locationName: string;
+  cost: string;
+  day: number;
+  maxStudents: number;
+  currentEnrollent: number;
+  studentsPerCoach: number;
+  lessonDates: LessonDate[];
+}
+interface LessonDate {
+  date: string;
+  startTime: null | string;
+  endTime: null | string;
 }
 
 export interface ILocation {
@@ -19,57 +20,5 @@ export interface ILocation {
   city: string
   district: string
   address: string
-  lessons: ILesson[]
 }
 
-
-export const MOCK_LESSON: ILesson = {
-  id: 1,
-  cost: '4회 18만원',
-  location: '역삼 아이스웍스',
-  day: 3,
-  maxStudents: 16,
-  currentEnrollment: 1,
-  studentsPerCoach: 4,
-  lessonDates: [
-    {
-      date: '2022-01-06',
-      startTime: '20:40',
-      endTime: '22:00'
-    },
-    {
-      date: '2022-01-13',
-      startTime: '20:40',
-      endTime: '22:00'
-    },
-    {
-      date: '2022-01-20',
-      startTime: '20:40',
-      endTime: '22:00',
-    },
-    {
-      date: '2022-01-27',
-      startTime: '20:40',
-      endTime: '22:00',
-    },
-  ]
-}
-
-export const MOCK_LOCATIONS: ILocation[] = [
-  {
-    id: 11,
-    name: "역삼 아이스웍스",
-    city: "서울특별시",
-    district: "강남구",
-    address: "역삼로 310 4층",
-    lessons: [MOCK_LESSON, MOCK_LESSON],
-  },
-  {
-    id: 10,
-    name: "신사 와이키키",
-    city: "서울특별시",
-    district: "강남구",
-    address: "압구정로 104 보암빌등",
-    lessons: [MOCK_LESSON, MOCK_LESSON, MOCK_LESSON],
-  }
-]
