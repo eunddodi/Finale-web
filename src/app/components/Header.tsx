140379649bfb9690008b41e03eb186e493950282
Claire Lee <claire.lee@crashco.io>
2024-07-12 18:46:49 +0900

    :sparkles: 헤더에 수강신청 버튼 추가

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -40,24 +40,26 @@ function Header() {
   return (
     <header className="w-full bg-main py-4 flex justify-between items-center px-8">
       <Link href='/' className="text-white text-lg font-semibold">FINALE</Link>
-      <nav className="hidden md:flex space-x-8 text-white text-xl font-bold">
-        <a href="/time-table" className="hover:underline">시간표</a>
-        <a href="/coaches" className="hover:underline">코치진</a>
-        <a href={NOTICE_NOTION_LINK} className="hover:underline">수업 별 안내</a>
-      </nav>
-      <button className="hidden md:block bg-white text-main-dark px-4 py-2 rounded-full text-xl font-bold">수강신청</button>
-      <div className="md:hidden relative">
-        <button id="hamburger" className="text-white" onClick={toggleDropdown}>
-          <i className="fas fa-bars"></i>
-        </button>
-        {isOpen && (
-          <div id="dropdown" className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
-            <a href="/time-table" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">시간표</a>
-            <a href="/coaches" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">코치진</a>
-            <a href={NOTICE_NOTION_LINK} className="block px-4 py-2 text-gray-800 hover:bg-gray-100">수업 별 안내</a>
-            <a href="/lessons" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">수강신청</a>
-          </div>
-        )}
+      <div className="flex items-center gap-8">
+        <nav className="hidden md:flex space-x-8 text-white text-xl font-semibold">
+          <a href="/time-table" className="hover:underline">시간표</a>
+          <a href="/coaches" className="hover:underline">코치진</a>
+          <a href={NOTICE_NOTION_LINK} className="hover:underline">수업 별 안내</a>
+        </nav>
+        <Link href='/lessons' className=" bg-white text-main-dark px-4 py-1 rounded-full text-lg font-semibold">수강신청</Link>
+        <div className="md:hidden relative">
+          <button id="hamburger" className="text-white" onClick={toggleDropdown}>
+            <i className="fas fa-bars"></i>
+          </button>
+          {isOpen && (
+            <div id="dropdown" className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg z-10">
+              <a href="/time-table" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">시간표</a>
+              <a href="/coaches" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">코치진</a>
+              <a href={NOTICE_NOTION_LINK} className="block px-4 py-2 text-gray-800 hover:bg-gray-100">수업 별 안내</a>
+              <a href="/lessons" className="block px-4 py-2 text-gray-800 hover:bg-gray-100">수강신청</a>
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
