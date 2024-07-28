cff95169c31c5bb9fa8d0c3ffe40bd89aa1e3226
Claire Lee <claire.lee@crashco.io>
2024-07-28 22:43:33 +0900

    :sparkles: 타이틀, 파비콘 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en" className={pretendard.className}>
       <head>
+        <title>피날레 - 성인 취미 피겨팀</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
         <QueryClientProvider>
