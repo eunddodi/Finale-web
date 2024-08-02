6fa832e9ff19a31db6b4407a683c4378270db6aa
Claire Lee <claire.lee@crashco.io>
2024-08-02 22:26:24 +0900

    :children_crossing: Show success toast when restLesson succeeds

--- src/hooks/mutations/useRestLessonMutation.ts
+++ src/hooks/mutations/useRestLessonMutation.ts
@@ -1,4 +1,5 @@
 import { restLesson } from "@/app/api";
+import { showSuccessToast } from "@/util";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function useRestLessonMutation() {
@@ -8,6 +9,7 @@ export default function useRestLessonMutation() {
     mutationFn: restLesson,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['lessons'] })
+      showSuccessToast("휴식 신청이 완료되었습니다.")
     }
   })
 }
--- src/util.tsx
+++ src/util.tsx
@@ -1,4 +1,6 @@
+import { toast } from 'react-toastify';
 import { LOCAL_STORAGE_KEYS } from './hooks/useLocalStorage';
+import { BadgeCheck } from 'lucide-react';
 
 export function formatDayOfWeek(dayOfWeek: number): string {
   const daysOfWeek: string[] = ['일', '월', '화', '수', '목', '금', '토'];
@@ -13,4 +15,14 @@ export function formatDayOfWeek(dayOfWeek: number): string {
 export function redirectToLogin() {
   window.location.href = '/login';
   localStorage.setItem(LOCAL_STORAGE_KEYS.REDIRECT_TO, JSON.stringify(window.location.pathname));
+}
+
+export function showSuccessToast(message: string) {
+  const SuccessToast = ({ message }: { message: string }) => (
+    <div className="flex items-center">
+      <BadgeCheck className="text-blue-500 mr-2" />
+      <span>{message}</span>
+    </div>
+  )
+  toast(<SuccessToast message={message} />)
 }
