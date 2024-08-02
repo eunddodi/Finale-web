80592df1e439ee8c7f8d88667cd2eca797666838
Claire Lee <claire.lee@crashco.io>
2024-08-02 17:17:31 +0900

    :construction: 서버에서 카카오 로그인 url 받아오도록 원복

--- src/app/api/index.tsx
+++ src/app/api/index.tsx
@@ -3,11 +3,12 @@ import { ILesson, ILocation, IMyLesson } from "./types"
 import { apiRequest } from "@/lib/api"
 
 export const login = async (type: 'student' | 'coach') => {
-  // const { data } = await apiRequest(`login/${type}`)
+  const { data } = await apiRequest(`login/${type}`)
+  redirect(data)
 
-  const KAKAO_LOGIN_URL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=3e7fdba27d8fb61d9c259d3a90ffc5a8&redirect_uri=https%3A%2F%2Ffinale-web.vercel.app%2Flogin%2Fcallback&state=student"
+  // const KAKAO_LOGIN_URL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=3e7fdba27d8fb61d9c259d3a90ffc5a8&redirect_uri=https%3A%2F%2Ffinale-web.vercel.app%2Flogin%2Fcallback&state=student"
   // TODO: url 변경으로 인해 로그인 안되는 경우 대비해 에러 처리 필요
-  redirect(KAKAO_LOGIN_URL)
+  // redirect(KAKAO_LOGIN_URL)
 }
 
 export const getLocationList = async (): Promise<ILocation[]> => {
@@ -38,4 +39,13 @@ export const enrollLesson = async ({ lessonId, token }: { lessonId: string, toke
 export const getMyLessons = async (token: string): Promise<IMyLesson[]> => {
   const { data } = await apiRequest('api/student/myPage', token)
   return data.reverse()
+}
+
+export const restLesson = async ({ lessonId, token }: { lessonId: string, token: string }) => {
+  await apiRequest(`api/student/restLesson/${lessonId}`, token)
+}
+
+export const checkApplyAvailability = async (): Promise<{ enrollment: boolean, restLesson: boolean }> => {
+  const { data } = await apiRequest('api/schedule/status')
+  return data
 }
