7102a5d10aeba7c8275ec2ac69ab68e961800366
Claire Lee <claire.lee@crashco.io>
2024-08-27 14:58:00 +0900

    :alien: 일요일을 7로 변경

--- src/util.tsx
+++ src/util.tsx
@@ -2,10 +2,10 @@ import { toast } from 'react-toastify';
 import { LOCAL_STORAGE_KEYS } from './hooks/useLocalStorage';
 
 export function formatDayOfWeek(dayOfWeek: number): string {
-  const daysOfWeek: string[] = ['일', '월', '화', '수', '목', '금', '토'];
+  const daysOfWeek: string[] = ['월', '화', '수', '목', '금', '토', '일'];
 
-  if (dayOfWeek < 0 || dayOfWeek > 6) {
-    throw new Error('올바른 요일 값이 아닙니다. 0부터 6 사이의 값을 입력해주세요.');
+  if (dayOfWeek < 1 || dayOfWeek > 7) {
+    throw new Error('올바른 요일 값이 아닙니다. 1부터 7 사이의 값을 입력해주세요.');
   }
 
   return daysOfWeek[dayOfWeek];
