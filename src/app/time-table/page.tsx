c910969e6542e20d81657a4e4a8daac90baf981d
Claire Lee <claire.lee@crashco.io>
2024-08-28 20:45:33 +0900

    :sparkles: 시간표 이미지 API로 조회해서 그리도록

--- src/app/time-table/page.tsx
+++ src/app/time-table/page.tsx
@@ -1,13 +1,34 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client'
+import Link from "next/link"
+import Loader from "../components/Loader"
+import ErrorFallback from "../components/ErrorFallback"
+import { useQuery } from "@tanstack/react-query"
+import { apiRequest } from "@/lib/api"
+
+const getTimetableImage = async (): Promise<string> => {
+  const res = await apiRequest('api/lesson/timetable')
+  return res.data
+}
 
 export default function SchedulePage() {
+  const { data: imageUrl, isLoading, error } = useQuery({
+    queryKey: ["timetableImage"],
+    queryFn: getTimetableImage
+  })
+
+  if (isLoading) return <Loader />
+  if (error) return <ErrorFallback />
+
   return (
     <div className="px-4 text-center mt-16 mb-32">
-      <Image src='/time-table/June/total.jpg' width={750} height={870} alt='Time table of June 2024' className='my-8' />
-      <Image src='/time-table/June/lessons.jpg' width={750} height={890} alt='Time table of June 2024' className='my-8' />
-      <Image src='/time-table/June/calendar.jpg' width={750} height={100} alt='Time table of June 2024' className='my-8' />
-      <Link href='/lessons' className="bg-sub-orange text-white px-6 py-2 rounded-full text-lg font-semibold">수강신청 하러 가기</Link>
+      {imageUrl && (
+        <div className="w-full max-w-xl mb-8 m-auto">
+          <img src={imageUrl} alt="Timetable" />
+        </div>
+      )}
+      <Link href='/lessons' className="bg-sub-orange text-white px-6 py-2 rounded-full text-lg font-semibold">
+        수강신청 하러 가기
+      </Link>
     </div>
-  );
+  )
 }
